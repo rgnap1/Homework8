7f9da58d3dfade19436b704cf47c13f6b84d56fb
rgnap <osovskaya_1993@mail.ru>
2022-11-29 18:38:57 +0300

    Change task

--- src/Main.java
+++ src/Main.java
@@ -3,157 +3,110 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Homework8 //task1.1");
-        //task1.1
+        System.out.println("//Homework8 //task1.1 //task2 //task3 //task4");
+        // task1.1
         int [] time = new int[3];
         time[0]=1;
-        System.out.println(time[0]);
         time[1]=2;
-        System.out.println(time[1]);
         time[2]=3;
-        System.out.println(time[2]);
+        for (int i=0;i<time.length;i++){
+            System.out.println(time[i]);
+        }System.out.println(" ");
 
+
+        //task2
+        for (int i =0;i<time.length;i++){
+            if(i==time.length-1){
+                System.out.print(time[i]);
+            } else {
+                System.out.print(time[i]+",");
+            }
+        }System.out.println(" ");
+
+
+        //task3
+        for (int i = time.length-1;i>=0;i--){
+            if (i!=0){
+                System.out.print(time[i]+",");
+            }else {
+                System.out.print(time[i]);
+            }
+        }System.out.println(" ");
         System.out.println(" ");
-        System.out.println("Homework8 //task1.2 ");
+
+
+        //task4
+        for (int i =0;i<time.length;i++){
+            if(i%2!=0){
+                System.out.println(time[i]);
+            }else {
+                System.out.println(time[i]+1);
+            }
+        }System.out.println(" ");
+
+
+
+        System.out.println("Homework8 //task1.2 //task2 //task3 ");
         //task1.2
         float [] apple = {1.57f, 7.654f, 9.986f};
         System.out.println(Arrays.toString(apple));
         System.out.println(apple.length);
-
-
         System.out.println(" ");
-        System.out.println("Homework8 //task1.3");
+
+
+        //task2
+        for (int i =0;i<apple.length;i++){
+            if(i==apple.length-1){
+                System.out.print(apple[i]);
+            }else {
+                System.out.print(apple[i]+",");
+            }
+        }System.out.println(" ");
+
+
+        //task3
+        for (int i = apple.length-1;i>=0;i--){
+            if(i!=0){
+                System.out.print(apple[i]+",");
+            }else {
+                System.out.print(apple[i]);
+            }
+        }System.out.println(" ");
+
+
+        System.out.println("Homework8 //task1.3 //task2 //task3 ");
         //task1.3
         int [] cats = new int[4];
         cats[0]= 1;
-        System.out.println(cats[0]);
         cats[1]=2;
-        System.out.println(cats[1]);
         cats[2]=3;
-        System.out.println(cats[2]);
         cats[3]=4;
-        System.out.println(cats[3]);
-
         System.out.println(" ");
-        System.out.println("Homework8 //task2");
+        for (int i=0;i<cats.length;i++){
+            System.out.println(cats[i]);
+        }System.out.println(" ");
+
+
         //task2
-        int [] time1 = new int[3];
-        time1[0]=1;
-        time1[1]=2;
-        time1[2]=3;
-        for (int i =0;i<time1.length;i++){
-            if(i==time1.length-1){
-                System.out.print(time1[i]);
-            } else {
-                System.out.print(time1[i]+",");
-            }
-        }
-        System.out.println(" ");
-        float [] apple1 = {1.57f, 7.654f, 9.986f};
-        for (int i =0;i<apple1.length;i++){
-            if(i==apple1.length-1){
-                System.out.print(apple1[i]);
+        for (int i=0;i<cats.length;i++){
+            if (i==cats.length-1){
+                System.out.print(cats[i]);
             }else {
-                System.out.print(apple1[i]+",");
+                System.out.print(cats[i]+",");
             }
-        }
-        System.out.println(" ");
-        int [] cats1 = new int[4];
-        cats1[0]= 1;
-        cats1[1]=2;
-        cats1[2]=3;
-        cats1[3]=4;
-        for (int i=0;i<cats1.length;i++){
-            if (i==cats1.length-1){
-                System.out.print(cats1[i]);
-            }else {
-                System.out.print(cats1[i]+",");
-            }
-        }
-        System.out.println(" ");
-        System.out.println("Homework8 //task3");
+        }System.out.println(" ");
+
+
         //task3
-        int [] time2 = new int[3];
-        time2[0]=1;
-        time2[1]=2;
-        time2[2]=3;
-        for (int i = time2.length-1;i>=0;i--){
+        for (int i = cats.length-1;i>=0;i--){
             if (i!=0){
-                System.out.print(time2[i]+",");
+                System.out.print(cats[i]+",");
             }else {
-                System.out.print(time2[i]);
-            }
-        }
-        System.out.println(" ");
-        float [] apple2 = {1.57f, 7.654f, 9.986f};
-        for (int i = apple2.length-1;i>=0;i--){
-            if(i!=0){
-                System.out.print(apple2[i]+",");
-            }else {
-                System.out.print(apple2[i]);
-            }
-        }
-        System.out.println(" ");
-        int [] cats2 = new int[4];
-        cats2[0]= 1;
-        cats2[1]=2;
-        cats2[2]=3;
-        cats2[3]=4;
-        for (int i = cats2.length-1;i>=0;i--){
-            if (i!=0){
-                System.out.print(cats2[i]+",");
-            }else {
-                System.out.print(cats2[i]);
-            }
-        }
-
-        System.out.println(" ");
-        System.out.println("Homework8 //task4");
-        //task4
-        int [] time3 = new int[3];
-        time3[0]=1;
-        time3[1]=2;
-        time3[2]=3;
-        for (int i =0;i<time3.length+1;i++){
-            if (i%2==0){
-                System.out.println(i);
-            }
-            else {
-                System.out.println(i+1);
+                System.out.print(cats[i]);
             }
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
